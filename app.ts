8647526b910a35d4d6b09441b6084e251fd004c5
Ibrohim <93027235+IbrohimFayzullayev@users.noreply.github.com>
2022-06-01 11:22:43 +0500

    learn unknown type

--- app.ts
+++ app.ts
@@ -1,32 +1,9 @@
-function add(n1: number, n2: number) {
-  return n1 + n2;
+let userInput: unknown;
+let userName: string;
+
+userInput = 5;
+userInput = "Max";
+
+if (typeof userInput === "string") {
+  userName = userInput;
 }
-
-function printResult(num: number): void {
-  console.log("Result is: " + num);
-}
-
-printResult(add(12, 7));
-
-// function types 👇 *** //
-
-// let combineValues: Function;
-let combineValues: (a: number, b: number) => number;
-
-combineValues = add;
-// combineValues = printResult;
-// combineValues = 6;
-console.log(combineValues(8, 8));
-
-// ******* //
-
-// function types and callbacks  ***** //
-
-function addAndHandle(n1: number, n2: number, cb: (num: number) => void) {
-  let result = n1 + n2;
-  cb(result);
-}
-
-addAndHandle(10, 20, (result) => {
-  console.log(result);
-});
