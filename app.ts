556b06e05ef56ec9be1cbbcb7ba672e25ec40912
Ibrohim <93027235+IbrohimFayzullayev@users.noreply.github.com>
2022-06-03 21:45:11 +0500

    ts

--- app.ts
+++ app.ts
@@ -1,3 +1,10 @@
-const userName = "Maximilian";
+let list: number[];
+list = [3, 4, 5];
 
-console.log(userName);
+for (let i of list) {
+  console.log(i);
+}
+
+for (let i in list) {
+  console.log(i);
+}
